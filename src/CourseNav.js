164faca145b748e0d4836ba32dd6f6b68f38c8b0
Adam Primarizki <adam.primarizki@gmail.com>
2020-06-25 16:14:50 +0700

    mobil stylesheet fix

--- src/CourseNav.js
+++ src/CourseNav.js
@@ -11,12 +11,15 @@ function CourseNav(){
     return (
         <div>
             <p className="course-name">{course.name}</p>
-            <div className='d-flex mb-4'>
-                <p className="course-headers mr-auto">Konten Kelas</p>
-                <p className="course-text mr-4">4 Materi | Total Durasi 10 min</p>
-            </div>
+            <Row className="mb-4 pr-4">
+                <Col md={6} xs={12} style={{padding:'0'}} className="pl-3">
+                    <p className="course-headers mr-auto">Konten Kelas</p>
+                </Col>
+                <Col md={6} xs={12} style={{padding:'0'}} className="pl-3">
+                    <p className="course-text">4 Materi | Total Durasi 10 min</p>
+                </Col>
+            </Row>
             <CourseContent title="Perkenalan"></CourseContent>
-
             <div className='d-flex justify-content-center align-items-center course-content-expand course-content-expand-selected mr-4'>
                 <p className="course-headers-expand mr-auto">{course.selectedContent.name}</p>
                 <img src={'/dropdown.png'} style={{ width: '10px', height: '7px' }} className="mr-3"></img> 
--- src/Footer.js
+++ src/Footer.js
@@ -10,44 +10,44 @@ function Footer(){
             <Col md={{span: 6, order: 'first'}} xs={{span: 12, order: 'last'}}>
                 <p className="course-headers-expand">PT DWI INTI PUTRA</p>
                 <Row className="mt-4">
-                    <Col md={{span: 6, order: 'first'}}>
+                    <Col md={{span: 6, order: 'first'}} className="mb-4">
                         <p className="course-headers-expand">Head Office</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Gedung Kresna</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Jl. Arjuna Utara No. 28</p>
                     </Col>
-                    <Col md={{span: 6, order: 'first'}}>
+                    <Col md={{span: 6, order: 'first'}} className="mb-4">
                         <p className="course-headers-expand">Marketing Office</p>
                         <p className="course-text" style={{fontSize: '16px'}}>BLOCK71 Jakarta</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Ariobimo Sentral</p>
                     </Col>
                 </Row>
                 <Row className="mt-4">
-                    <Col md={{span: 2, order: 'first'}}>
+                    <Col md={{span: 2, order: 'first'}} xs={{span: 4}}>
                         <p className="course-text" style={{fontSize: '16px'}}>Email:</p>
                     </Col>
-                    <Col md={{span: 10, order: 'first'}}>
+                    <Col md={{span: 10, order: 'first'}} xs={{span: 8}}>
                         <p className="course-headers-expand">hello@myedusolve.com</p>
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={{span: 2, order: 'first'}}>
+                    <Col md={{span: 2, order: 'first'}} xs={{span: 4}}>
                         <p className="course-text" style={{fontSize: '16px'}}>WhatsApp:</p>
                     </Col>
-                    <Col md={{span: 10, order: 'first'}}>
+                    <Col md={{span: 10, order: 'first'}} xs={{span: 8}}>
                         <p className="course-headers-expand">+62 877-8890-9020</p>
                     </Col>
                 </Row>
             </Col>
             <Col md={{span: 6, order: 'last'}} xs={{span: 12, order: 'first'}}>
                 <Row>
-                    <Col md={{span: 6, order: 'first'}}>
+                    <Col md={{span: 6, order: 'first'}} className="mb-4">
                         <p className="course-name" style={{fontSize: '24px'}}>MyEduSolve X</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Tentang Kami</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Kontak Kami</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Jadi Instruktur</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Faq</p>
                     </Col>
-                    <Col md={{span: 6, order: 'first'}}>
+                    <Col md={{span: 6, order: 'first'}} className="mb-4">
                         <p className="course-name" style={{fontSize: '24px'}}>Program</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Kelas</p>
                         <p className="course-text" style={{fontSize: '16px'}}>Events</p>
@@ -57,19 +57,21 @@ function Footer(){
                 </Row>
             </Col>
         </Row>
-        <Row style={{backgroundColor: 'white', padding: '28px 180px'}}>
-            <Col md={{span: 4}} xs={{span: 12}}>
+        <Row  className="appFooter" style={{backgroundColor: 'white'}}>
+            <Col md={{span: 4}} xs={{span: 12}} className="term-privacy">
                 <div className="d-flex">
-                    <p className="course-text mr-auto">Term and Conditions</p>
-                    <p className="course-text">Privacy and Policy</p>
+                    <p className="course-text mr-auto" style={{color:'black'}}>Term and Conditions</p>
+                    <p className="course-text" style={{color:'black'}}>Privacy and Policy</p>
                 </div>
             </Col>
             <Col md={{span: 8}} xs={{span: 12}}>
-                <div className="d-flex">
-                    <img src={'/twitter.png'} className="ml-auto"></img> 
-                    <img src={'/facebook.png'} className="ml-5"></img> 
-                    <img src={'/instagram.png'} className="ml-5"></img> 
-                    <img src={'/youtube.png'} className="ml-5"></img> 
+                <div className="footer-logo">
+                    <div>
+                        <img src={'/twitter.png'} className="twitter"></img> 
+                        <img src={'/facebook.png'} className="ml-5"></img> 
+                        <img src={'/instagram.png'} className="ml-5"></img> 
+                        <img src={'/youtube.png'} className="ml-5"></img> 
+                    </div>
                 </div>
             </Col>
         </Row>
